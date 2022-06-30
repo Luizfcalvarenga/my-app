7407d212a380159b92adde90c489194cb6e2e4da
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-06-30 20:30:46 -0300

    tutorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Would only recommend doing this though if you know exactly what was changed in the repo and you're 100% confident you want the local commit to replace everything. Mine was a hobby project that no one else is working on and no other commits had been made from any other devices aside from the manual changes.
+      </div>
+    );
+  }
+}
+
+export default App
